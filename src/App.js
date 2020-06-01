6a61b7dad290d2124c820a9df05828a634603bfb
Hai Duc Vu <haidv@hitachiconsulting.com>
2020-06-01 18:07:30 +0700

    update component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import React, { Component } from "react";
-
+import Header from "./components/header";
 class App extends Component {
   render() {
     return (
       <div>
-        <h1>AppComponent</h1>
-        <p>Ngày 31/5/2020</p>
+        {/* <h1>AppComponent</h1>
+        <p>Ngày 31/5/2020</p> */}
+        <Header />
       </div>
     );
   }
