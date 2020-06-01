7d95c24f496ff794fff02d0a1d7ea4a4462f08be
haiducvu <duchaivu1997@gmail.com>
2020-06-01 08:38:06 +0700

    update component

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React, { Component } from "react";
+
+class App extends Component {
+  render() {
+    return (
+      <div>
+        <h1>AppComponent</h1>
+        <p>Ngày 31/5/2020</p>
+      </div>
+    );
+  }
+}
+export default App;
