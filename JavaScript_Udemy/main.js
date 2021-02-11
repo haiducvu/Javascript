74d671c4b67ec2d5966389778dfb333fecd47da1
Hai Vu <duchaivu1997@gmail.com>
2021-02-11 20:04:18 +0700

    practice js in question game

--- JavaScript_Udemy/main.js
+++ JavaScript_Udemy/main.js
@@ -365,23 +365,63 @@
   //10. IMMEDIATELY INVOKED FUNCTION EXPRESSTION  
   
   //11. CLOSURES
-  function retirement(retirementAge){
-    a= 'years left until retirement.';
-    return function(yearofBirth){
-      var age= 2021- yearofBirth;
-      console.log((retirementAge- age)+a);
+//   function retirement(retirementAge){
+//     a= 'years left until retirement.';
+//     return function(yearofBirth){
+//       var age= 2021- yearofBirth;
+//       console.log((retirementAge- age)+a);
+//     }
+//   }
+
+//   let retirementUS= retirement(66); 
+//   let retimentGemeny= retirement(65);
+//   let retimentIceland= retirement(64);
+
+//  retirementUS(2021);
+//  retirement(66)(2021);
+
+  
+// 12. Code Challengen
+
+(function(){
+  function Question(question, answers, correct){
+    this.question= question;
+    this.answers= answers;
+    this.correct= correct;
+  }
+  
+  Question.prototype.displayQuestion = function() {
+    console.log(this.question);
+  
+    for (var i = 0; i < this.answers.length; i++) {
+        console.log(i + ': ' + this.answers[i]);
     }
   }
-
-  let retirementUS= retirement(66); 
-  let retimentGemeny= retirement(65);
-  let retimentIceland= retirement(64);
-
- retirementUS(2021);
- retirement(66)(2021);
-
-  //12. Bind, Call and Apply
-
-
-
- 
+  
+  Question.prototype.checkAnwers= function (ans){
+    if(ans=== this.correct){
+      console.log('Correct answers!');
+    }else{
+      console.log('Wrong answer, Try again');
+    }
+  }
+  
+  var q1= new Question('In JavaScript is language programming?', ['Yes', 'No'], 0);
+  
+  var q2= new Question('Who is winner of the award?', ['John', 'Micheal', 'Thomas'], 2);
+  
+  var q3= new Question('In C++ is language programming?', ['Yes', 'No'], 1);
+  
+  var q4= new Question('Who is singer?', ['John', 'Micheal', 'Thomas'], 3);
+  
+  var questions=[q1, q2, q3, q4];
+  
+  var n= Math.floor(Math.random()* questions.length);
+  console.log(n); 
+  
+  questions[n].displayQuestion();
+  
+  var answer= parseInt(prompt('Please select the correct answer'));
+  
+  questions[n].checkAnwers(answer);
+})();
