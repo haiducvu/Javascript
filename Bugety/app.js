aad39af11a879559ed9d34a4f35de8de0beb2bf1
Hai Vu <duchaivu1997@gmail.com>
2021-02-21 14:41:47 +0700

    9. creating income and expense Function

--- Bugety/app.js
+++ Bugety/app.js
@@ -3,8 +3,36 @@
 
 // BUTGET CONTROLLER
 var budgetController= (function(){
-  // Some code 
-});
+    
+    // this is function constructor 
+    var Expense= function (id, description, value){
+        this.id= id;
+        this.description= description;
+        this.value= value;
+    };
+
+    var Income= function (id, description, value){
+        this.id= id;
+        this.description= description;
+        this.value= value;
+    };
+
+    var allExpensive =[];
+    var allIncomes= [];
+    var totalExpense=0;
+
+    var data={
+        allItems:{
+            exp: [],
+            inc: [],
+        },
+        totals:{
+            exp: 0,
+            inc: 0,
+        } 
+    }
+
+})();
 
 
 //UI CONTROLLER
