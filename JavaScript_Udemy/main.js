272b2af83fe1e9f4fc34d21cb2e6b94ccc74ea56
Hai Vu <duchaivu1997@gmail.com>
2021-03-09 22:53:36 +0700

    update practice interview

--- JavaScript_Udemy/main.js
+++ JavaScript_Udemy/main.js
@@ -322,6 +322,7 @@
   // }
 
   // function calculateAge(el){
+  //   console.log(el);
   //   return 2016- el;
   // }
 
@@ -461,3 +462,253 @@
 
 })();
 */
+
+
+//  CALL, BIND, APPLY
+// var john = {
+//   name: 'John',
+//   age: 26,
+//   job: 'teacher',
+//   presentation: function(style, timeOfDay) {
+//       if (style === 'formal') {
+//           console.log('Good ' + timeOfDay + ', Ladies and gentlemen! I\'m ' +  this.name + ', I\'m a ' + this.job + ' and I\'m ' + this.age + ' years old.');
+//       } else if (style === 'friendly') {
+//           console.log('Hey! What\'s up? I\'m ' +  this.name + ', I\'m a ' + this.job + ' and I\'m ' + this.age + ' years old. Have a nice ' + timeOfDay + '.');
+//       }
+//   }
+// };
+
+// var emily = {
+//   name: 'Emily',
+//   age: 35,
+//   job: 'designer'
+// };
+
+// // john.presentation('formal', 'morning');
+
+// // john.presentation.call(emily, 'friendly', 'afternoon');
+
+// //john.presentation.apply(emily, ['friendly', 'afternoon']);
+
+// var johnFriendly = john.presentation.bind(john, 'friendly');
+
+// johnFriendly('morning');
+// johnFriendly('night');
+
+// var emilyFormal = john.presentation.bind(emily, 'formal');
+// emilyFormal('afternoon');
+
+  
+
+// binding trong arrow function
+// let me={
+//   name:"hai",
+//   x:()=>{ console.log("My name "+ this.name)},
+//   y:function(){
+//     console.log("My name "+ this.name);
+//   },
+
+//   z: function (){
+//     // function foo(){
+//     //   console.log(this.name);
+//     // }
+//     let foo=()=>{
+//       console.log(this.name)
+//     }
+//     foo();
+//   }
+
+// }
+
+// me.x();
+// me.y();
+// me.z();
+// argument trong arrow function
+// let myFunc={
+//   showNor(){
+//     console.log(arguments);
+//   },
+//   showArr:()=>{console.log(arguments)}}
+
+// myFunc.showNor(1, 2, 3);
+// myFunc.showArr(1, 2, 3);
+
+// new trong arr function
+// const foo=()=>{}
+
+// function foo(){
+
+// }
+// const bar= new foo();
+// bar.hello= "Hello";
+// console.log(bar.hello); 
+
+//Resparameter
+// function sum(...theArges){
+//   return theArges.reduce((previous, current)=>{
+//     return previous+ current; 
+//   }); 
+// }
+// console.log(sum(1,2,3));
+
+
+//Template String
+// `string text`
+// console.log(`string line 1
+// string line 2`);
+
+ 
+
+
+// for(let i=1; i< arr.length; i++){
+//   let max=arr[0];
+//   if(max< arr[i]){
+//     max=arr[i];
+//   }
+//   return max;
+// }
+
+ 
+// const arr=[1,2,3,4,5];
+
+// const a = [...arr]; // 1 2 3 4 5  
+// const b= [...arr];
+// b.push(7,8,9);
+// console.log(arr,b);
+
+// let result=[];
+// function findNumber(arr, result){
+//   for(let i=0; i<arr.length; i++){
+//     if(arr[i]%2==0){
+//       result.push(arr[i]);
+//     }
+//   }
+//   return result;
+// }
+
+// console.log(findNumber(arr, result));
+
+// const arrResult=arr.filter((item)=>{
+//   return item%2===0;
+// })
+// console.log(arrResult);
+
+// const sayHello = (name, callback) => {
+//  const msg = "Hello, " + name;
+//  return callback(msg);
+// };
+//  
+// const useCallBack = sayHello("Khoa", (msg) => {
+//  return msg;
+// });
+//  
+// console.log(useCallBack);
+
+
+ 
+// function swapNumber(a,b){
+  // let c=a+b;
+  // a=c-a;
+  // b=c-b;
+  // console.log(a,b);
+  //a=1, b=5;  
+  //   a=a+b;  //a=6;
+  //   b=a-b;  //b=6-5=1
+  //   a=a-b;  //a=6-1=5
+  // console.log(a,b);
+  // [x,y] = [b,a];
+  // console.log(x,y);
+// 
+// }
+// swapNumber(1,5);
+ 
+// console.log('this',this);
+
+// function funcThis(){
+//   console.log('thisInFunction', this);
+// }
+
+// funcThis();
+
+                            //Template Literals (Template String) 
+
+  //template literal ko dùng dấu +,-
+// const courseName= 'JavaScript';
+// const description= 'Course Name ' + courseName;
+
+// console.log(description);
+
+// const courseName= 'JavaScript';
+// const courseName1= 'JavaScript';
+// const description= `Course Name: ${courseName} ${courseName1}`;
+// //quy tắc `\` => `\\`;
+// //quy tắc xuống dòng => `\n`;
+// const ption= `Template String: \${}`
+// console.log(ption);
+
+// const  lines= 'Line 1\nLine 2';
+
+// console.log(lines);
+
+//kiem tra so nguyen to
+// function checkSonguenTo(n){
+//   if(n===1 || n<1){
+//     return true;
+//   }
+//   for(let x=2; x< n/2;x++){
+//     if(n%x ===0){
+//       return false;
+//     }
+//   }
+//   return true;
+// }
+//  console.log(checkSonguenTo(18));
+
+
+//Promise
+      //CALLBACK HELL
+// setTimeout(function(){
+//   console.log(1);
+//   setTimeout(function(){
+//     console.log(2);
+//   },1000)
+// },1000)
+
+
+// 1. new promise
+// 2. Executor
+
+
+// Trạng thái 1: Pending đang chờ thành công or thất bại
+// Trạng thát 2: Fullfilled-> thành công
+// Trạng thát 3: Reject-> thất bại
+// var promise= new Promise(
+//    //Executor
+//    function(resolve, reject){
+//       // Logic
+//       // Thành công =>resolve()
+//       // Thất bại=> reject()
+//       reject();
+//    }
+// )
+
+// promise
+//    .then(function(){
+//       console.log('Successully');
+//    })
+//    .catch(function(){
+//     console.log('Failure');
+//    })
+//    .finally(function(){
+//     console.log('Done');
+//   })
+
+ function sleep(ms){
+   return new Promise(function(resolve){
+     setTimeout(resolve, ms);
+   })
+ }
+
+ sleep(1000).then(function(){
+   console.log(1);
+ })
